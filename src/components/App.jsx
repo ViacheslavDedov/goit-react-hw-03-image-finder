c3f926660ab69312f1c36b3779b91aa7eb8d915e
ViacheslavDedov <dedoff.v@gmail.com>
2022-07-15 14:13:40 +0300

    add components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,13 @@
+// import { Component } from 'react';
+// import { Container } from './Container/Container';
+// import { Searchbar } from './Searchbar/Searchbar';
+// import { ImageGallery } from './ImageGallery/ImageGallery';
+// import { Button } from './Button/Button';
+// import { Modal } from './Modal/Modal';
+// import { Loader } from './Laoder/Loader';
+// import { getImages } from 'services/imageSearchApi';
+
+
 export const App = () => {
   return (
     <div
